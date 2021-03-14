07ace174802b6b888f2ff4cd11c55296c8429a53
zoulingbin <40159060+zoulingbin@users.noreply.github.com>
2021-03-15 02:49:42 +0800

    queue

--- queue/queue.go
+++ queue/queue.go
@@ -11,26 +11,24 @@ var(
 	Size = 0
 	queue = &Node{nil,nil}
 )
-func(n *Node) Push(value interface{}) bool{
+func(n *Node) Push(value interface{}){
 	if queue == nil {
 		queue = &Node{value, nil}
-		Size ++
-		return true
+	}else{
+		n.Value = value
+		n.Next = queue
 	}
-	n.Value = value
-	n.Next = queue
 	Size++
-	return true
 }
 
-func(n *Node) Pop() (interface{}, bool){
+func(n *Node) Pop() (interface{}){
 	if Size == 0 {
-		return 0,false
+		return 0
 	}
 	if Size == 1{
 		queue = nil
 		Size--
-		return n.Value,true
+		return n.Value
 	}
 	temp := n
 	for (n.Next) != nil{
@@ -38,11 +36,11 @@ func(n *Node) Pop() (interface{}, bool){
 		n = n.Next
 	}
 	Size --
-	return temp.Next.Value, true
+	return temp.Next.Value
 }
 
 //遍历节点
-func (n *Node)traverse() {
+func (n *Node)Traverse() {
 	if Size == 0 {
 		fmt.Println("empty Queue!")
 	}
@@ -50,4 +48,56 @@ func (n *Node)traverse() {
 		fmt.Printf("%v-> ",n.Value)
 		n = n.Next
 	}
+}
+
+/**
+方法2
+ */
+type ListNode struct {
+	Val		interface{}
+	Next 	*ListNode
+}
+
+type ListQueue struct {
+	head 	*ListNode
+	tail 	*ListNode
+	length 	int
+}
+
+func NewQueue() *ListQueue {
+	return new(ListQueue)
+}
+
+func (l *ListQueue)Push(value interface{}) {
+	node := &ListNode{value, nil}
+	if l.tail == nil {
+		l.head = node
+		l.tail = node
+	} else {
+		l.tail.Next = node
+		l.tail = node
+	}
+	l.length ++
+}
+
+func (l *ListQueue)Pop() (value interface{}) {
+	if l.head == nil {
+		return
+	}
+	value = l.tail.Val
+	l.head = l.head.Next
+	l.length --
+	return
+}
+
+func(l *ListQueue) Traverse() {
+	if l.head == nil {
+		fmt.Printf("empty Queue!")
+	}else{
+		for cur := l.head; cur != nil; cur = cur.Next{
+			fmt.Printf(" <-%+v",cur.Val)
+		}
+
+	}
+
 }
