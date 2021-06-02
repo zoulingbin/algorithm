99f58da72da30ffc026caf525137ae9701c566ad
zoulingbin <466483158@qq.com>
2021-06-02 16:27:54 +0800

    day8 反转链表

--- list/reverse.go
+++ list/reverse.go
@@ -1,16 +1,50 @@
 package list
 
+import "fmt"
+
 type ListNode struct {
-	Value 	interface{}
+	Val 	int
 	Next 	*ListNode
 }
 
-func (l *ListNode) Reverse() *ListNode{
-	cur := l
+func Reverse(head *ListNode) *ListNode{
+	cur := head
 	var pre *ListNode
 
 	for cur != nil{
 		cur.Next, pre, cur = pre, cur, cur.Next
 	}
-	return pre
+	return head
+}
+
+//构建链表
+func convertListNode(s []int) *ListNode {
+	sLen := len(s)
+	if sLen == 0 {
+		return nil
+	}
+	listNode := &ListNode{
+		Val: s[0],
+	}
+	temp := listNode
+	for i := 1; i < sLen; i++ {
+		temp.Next = &ListNode{
+			Val: s[i],
+		}
+		temp = temp.Next
+	}
+	return listNode
+}
+
+//打印链表
+func (l *ListNode) printListNode() {
+	var s []int
+	pointer := l
+	for pointer != nil {
+		s = append(s, pointer.Val)
+		pointer = pointer.Next
+	}
+	for _, v := range s {
+		fmt.Println(v)
+	}
 }
